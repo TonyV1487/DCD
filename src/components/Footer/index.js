caac3445424c21f017647ebbc344796c21f0d931
TonyV <tvester1487@gmail.com>
2020-07-26 17:19:40 -0500

    Correct Width

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,42 +12,40 @@ import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <Container flex-wrap>
-      <Navbar bg="dark" variant="dark">
-        <Col>
-          <Row className="justify-content-md-center">
-            <p className="ft-title">Find us on Social Media</p>
-          </Row>
-          <Row className="justify-content-md-center">
-            <FontAwesomeIcon icon={faInstagram} size="2x" color="white" />
-            <FontAwesomeIcon icon={faFacebook} size="2x" color="white" />
-            <FontAwesomeIcon icon={faYoutube} size="2x" color="white" />
-            <FontAwesomeIcon icon={faYelp} size="2x" color="white" />
-          </Row>
-        </Col>
-        <Col>
-          <Row className="justify-content-md-center">
-            <p className="ft-title">Visit Us</p>
-          </Row>
-          <Row className="justify-content-md-center">
-            <p className="ft-text">
-              841 E Wilson St<br></br>
-              Batavia, IL 60510
-            </p>
-          </Row>
-        </Col>
-        <Col>
-          <Row className="justify-content-md-center">
-            <FontAwesomeIcon icon={faPhone} size="lg" color="white" />
-            <p className="ft-title">Call Us - (555) 555-5555</p>
-          </Row>
-          <Row className="justify-content-md-center">
-            <FontAwesomeIcon icon={faEnvelope} size="lg" color="white" />
-            <p className="ft-title">Email Us - info@dcdtl.com</p>
-          </Row>
-        </Col>
-      </Navbar>
-    </Container>
+    <Navbar bg="dark" variant="dark">
+      <Col>
+        <Row className="justify-content-md-center">
+          <p className="ft-title">Find us on Social Media</p>
+        </Row>
+        <Row className="justify-content-md-center">
+          <FontAwesomeIcon icon={faInstagram} size="2x" color="white" />
+          <FontAwesomeIcon icon={faFacebook} size="2x" color="white" />
+          <FontAwesomeIcon icon={faYoutube} size="2x" color="white" />
+          <FontAwesomeIcon icon={faYelp} size="2x" color="white" />
+        </Row>
+      </Col>
+      <Col>
+        <Row className="justify-content-md-center">
+          <p className="ft-title">Visit Us</p>
+        </Row>
+        <Row className="justify-content-md-center">
+          <p className="ft-text">
+            841 E Wilson St<br></br>
+            Batavia, IL 60510
+          </p>
+        </Row>
+      </Col>
+      <Col>
+        <Row className="justify-content-md-center">
+          <FontAwesomeIcon icon={faPhone} size="lg" color="white" />
+          <p className="ft-title">Call Us - (555) 555-5555</p>
+        </Row>
+        <Row className="justify-content-md-center">
+          <FontAwesomeIcon icon={faEnvelope} size="lg" color="white" />
+          <p className="ft-title">Email Us - info@dcdtl.com</p>
+        </Row>
+      </Col>
+    </Navbar>
   );
 }
 
