1c4a257ddf08b285cf191b316040bc355445e4e7
TonyV <tvester1487@gmail.com>
2020-07-29 21:05:24 -0500

    Init services page

---
+++ src/pages/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Services = () => {
+  return (
+    <>
+      <Row>
+        <Col>Hello world!</Col>
+      </Row>
+    </>
+  );
+};
+
+export default Services;
